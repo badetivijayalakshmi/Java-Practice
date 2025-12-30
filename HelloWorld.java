147c4c0bb4d15f0abd9e3ece53feaa54dc02a4df
Badeti Vijaya Lakshmi <badetivlakshmi18@gmail.com>
2025-12-30 19:42:23 +0530

    Implement sum and even/odd check in HelloWorld.java
    Added functionality to calculate the sum of two numbers and check if a number is even or odd.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,3 +4,32 @@ public class Main{
         System.out.println("Hello World");
     }
 }
+
+
+//Sum of two numbers by taking input from the user
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("Sum is:"+sum);
+    }
+
+}
+
+//Even or odd check
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n%2 == 0){
+            System.out.println("Even number");
+        } else{
+            System.out.println("Odd number");
+        }
+      
+    }
+}
