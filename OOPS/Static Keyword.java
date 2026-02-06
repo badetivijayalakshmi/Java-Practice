ce3fc12dcd3737999e7c3bb8c00b26c285732bfa
Badeti Vijaya Lakshmi <badetivlakshmi18@gmail.com>
2026-02-06 22:47:01 +0530

    Static keyword in Java
    Demonstrate the use of static variables in a class.

---
+++ OOPS/Static Keyword.java
@@ -0,0 +1,23 @@
+//Static Keyword-Static can be : Variable (also known as a class variable),Method (also known as a class method),Block,Nested class
+//Code
+class Student {
+   static String school;
+   String name;   
+}
+
+
+public class OOPS {
+   public static void main(String args[]) {
+       Student.school = "JMV";//can be directly accessed using the class name without object creation
+       Student s1 = new Student();
+       Student s2 = new Student();
+
+
+       s1.name = "Meena";
+       s2.name = "Beena";
+
+
+       System.out.println(s1.school);
+       System.out.println(s2.school);
+   }
+}
