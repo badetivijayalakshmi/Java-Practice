97ff9b3092fa11d6589fadf32682b22417847cca
Badeti Vijaya Lakshmi <badetivlakshmi18@gmail.com>
2026-02-23 19:44:26 +0530

    Implement runtime polymorphism with payment classes

--- OOPS/Polymorphism/Runtime Polymorphism.java
+++ OOPS/Polymorphism/Runtime Polymorphism.java
@@ -39,3 +39,43 @@ public class Java{
 //Paid using Credit Card: 5000.0
 //Paid using UPI: 3000.0
 //Paid using Cash: 100.0
+//example-2
+class Parent{
+    double amount;
+    public void pay(double amount){
+        System.out.println("Processing payment");
+    }
+}
+
+class CreditCard extends Parent{
+    public void pay(double amount){
+        System.out.println("Paid using Credit Card: " + amount);
+    }
+}
+class UPI extends Parent{
+    public void pay(double amount){
+        //System.out.println("Paid using UPI: " + amount);
+        super.pay(amount);
+    }
+}
+class Cash extends Parent{
+    public void pay(double amount){
+        System.out.println("Paid using Cash: " + amount);
+    }
+}
+
+public class Java{
+    public static void main(String[] args) {
+        Parent p;
+        p = new CreditCard();
+        p.pay(5000);
+        p = new UPI();
+        p.pay(3000);
+        p = new Cash();
+        p.pay(100);
+    }
+}
+//o/p:
+//Paid using Credit Card: 5000.0
+//Processing payment
+//Paid using Cash: 100.0
